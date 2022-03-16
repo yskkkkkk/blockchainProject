008887e94f81dc5fa684c0824f773cfc65dbdea6
KIM Tae Hun <gift2barry@gmail.com>
2022-03-16 20:51:52 +0900

    design: Add temporary footer and search box

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
