2d6edb20135a8b6984343a6afd6ac9130f3a28dc
pangE <cdpkct@gmail.com>
2022-03-18 18:31:09 +0900

    feat: add 207 API

--- back/src/main/java/com/ssafy/woori/domain/funding/service/FundingServiceImpl.java
+++ back/src/main/java/com/ssafy/woori/domain/funding/service/FundingServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ssafy.woori.domain.funding.service;
 
 import com.ssafy.woori.domain.funding.dao.FundingDao;
+import com.ssafy.woori.domain.funding.dto.FundingInfoResponse;
 import com.ssafy.woori.domain.funding.dto.FundingListResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FundingServiceImpl implements FundingService{
@@ -17,5 +19,24 @@ public class FundingServiceImpl implements FundingService{
     public List<FundingListResponse> fundingHot() {
 
         return (fundingDao.findByEmailAdd());
+    }
+
+    @Override
+    public Optional<FundingInfoResponse> fundingInfo(int fundingSeq) {
+
+        Optional<FundingInfoResponse> response = fundingDao.findByFundingSeq(fundingSeq);
+
+        //if(response.isPresent()) return (response);
+
+//        Optional<Funding> funding = fundingDao.findById(fundingSeq);
+//
+//        FundingInfoResponse response;// = FundingInfoResponse.builder().build();
+//
+//        funding.ifPresent(selectFunding ->{
+//
+//        });
+
+
+        return response;
     }
 }
