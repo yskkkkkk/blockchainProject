27bebccbe61bd67181ed4eb3532ac02e978ecdcf
pangE <cdpkct@gmail.com>
2022-03-18 16:52:58 +0900

    fix: delete test code

--- back/src/main/java/com/ssafy/woori/domain/funding/service/FundingServiceImpl.java
+++ back/src/main/java/com/ssafy/woori/domain/funding/service/FundingServiceImpl.java
@@ -1,10 +1,7 @@
 package com.ssafy.woori.domain.funding.service;
 
 import com.ssafy.woori.domain.funding.dao.FundingDao;
-import com.ssafy.woori.domain.funding.dao.HotDao;
-import com.ssafy.woori.domain.funding.dto.FundingListRequest;
 import com.ssafy.woori.domain.funding.dto.FundingListResponse;
-import com.ssafy.woori.entity.Hot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
