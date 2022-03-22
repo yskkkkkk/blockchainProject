2322d4d7f824c13bd8936cb0ed81c4427b447133
KIM Tae Hun <gift2barry@gmail.com>
2022-03-22 22:06:10 +0900

    feat: Add temporary main fund page for testing the dynamic urls of detail product pages ver.2

---
+++ front/pages/products/index.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+export const getStaticProps = async () => {
+
+  const res = await fetch('https://retoolapi.dev/X9nA53/dummy');
+  const data = await res.json();
+  
+  return {
+    props: {funds: data}
+  }
+
+}
+
+
+const Products = ({funds}) => {
+
+  return (
+    <div>
+      <h1>전체 펀드 상품들</h1>
+      {funds.map(fund => (
+        <Link href={'/products/' + fund.id} key={fund.id}>
+          <h3>{fund.col1}</h3>
+          <p>fund.fullName</p>
+        </Link>
+      ))}
+    </div>
+  )
+
+}
