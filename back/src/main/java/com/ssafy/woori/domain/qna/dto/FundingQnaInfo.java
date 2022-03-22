e0d64209e245c78fecda6d71d6b20eb71dd0dbee
pangE <cdpkct@gmail.com>
2022-03-22 13:35:31 +0900

    feat: make fundingQnaInfo DTO

--- back/src/main/java/com/ssafy/woori/domain/qna/dto/FundingQnaInfo.java
+++ back/src/main/java/com/ssafy/woori/domain/qna/dto/FundingQnaInfo.java
@@ -1,2 +1,28 @@
-package com.ssafy.woori.domain.qna.dto;public interface FundingQnaInfo {
+package com.ssafy.woori.domain.qna.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+//public interface FundingQnaInfo {
+//    //String getUserNickname();
+//    String getQnaText();
+//    LocalDate getQnaCreatedDate();
+//    String getReplyText();
+//    LocalDate getReplyCreatedDate();
+//    boolean isSecret();
+//}
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class FundingQnaInfo{
+    private String userNickname;
+    private String qnaText;
+    private LocalDate qnaCreatedDate;
+    private String replyText;
+    private LocalDate replyCreatedDate;
+    private boolean secret;
 }
