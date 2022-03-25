7bc9bc662db23940f3f3820c27386a3849f48f68
KIM Tae Hun <gift2barry@gmail.com>
2022-03-25 23:05:30 +0900

    feat: Add transition while changing routes

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,15 +1,29 @@
+import {motion} from 'framer-motion';
 import '../styles/globals.css'
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <>
-      <div className="grid grid-cols-1 gap-[5rem] mx-[12rem]">
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-      </div>
+      <motion.div
+        key={router.route}
+        initial="initial"
+        animate="animate"
+        variants={{
+          initial: {
+            opacity: 0,
+          },
+          animate: {
+            opacity: 1,
+          },
+        }}>
+        <div className="grid grid-cols-1 gap-[5rem] mx-[12rem]">
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </div>
+      </motion.div>
     </>
   )
 }
