5ab3442b554bd2568429237cd2fb58c4dd12ade4
KIM Tae Hun <gift2barry@gmail.com>
2022-03-30 16:50:04 +0900

    refactor: Add semantic tags on qna and announcement modals

--- front/components/qnaModal/index.js
+++ front/components/qnaModal/index.js
@@ -1,4 +1,3 @@
-import { set } from "date-fns";
 import {motion} from "framer-motion";
 import { useState } from "react";
 
@@ -79,26 +78,26 @@ export default function QnaModal({addQna, handleClose}) {
             </label>
           </div>
         </header>
-        <div className="my-4">
+        <section className="my-4">
           <label className="block mb-2 text-sm font-bold text-gray-700" for="title">
             제목
           </label>
           <input onChange={(e) => setTitle(e.target.value)} className="w-full px-3 py-2 text-gray-700 border rounded shadow focus:outline-none focus:shadow-theme-color" id="title" type="text" />
-        </div>
-        <div className="mb-6">
+        </section>
+        <section className="mb-6">
           <label className="block mb-2 text-sm font-bold text-gray-700" for="content">
             질문 내용
           </label>
           <textarea onChange={(e) => setContent(e.target.value)} className="w-full px-3 py-2 mb-3 text-gray-700 border rounded shadow focus:outline-none focus:shadow-theme-color" id="content" />
-        </div>
-        <div className="flex items-center justify-between">
+        </section>
+        <section className="flex items-center justify-between">
           <button onClick={submitQna} className="px-4 py-2 font-bold text-white rounded bg-theme-color/80 hover:bg-theme-color focus:outline-none focus:shadow-outline">
             작성
           </button>
           <button onClick={cancel} className="inline-block text-sm font-bold align-baseline text-theme-color hover:text-blue-800" href="#">
             취소
           </button>
-        </div>
+        </section>
       </form>
     </motion.div>
   )
