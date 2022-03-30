97530f7b175d20bbdb0a2dafb3de96be6ed3d609
KIM Tae Hun <gift2barry@gmail.com>
2022-03-31 00:45:02 +0900

    refactor: Convert product detail page basic info section into component

---
+++ front/pages/order.js
@@ -0,0 +1,6 @@
+export default function Order() {
+  
+  return (
+    <h1 className="flex justify-center align-middle">this is order page</h1>
+  )
+}
--- front/pages/products/[id].js
+++ front/pages/products/[id].js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import Image from 'next/image';
 
+import ProductBasics from "../../components/productBasics";
 import ProductDetail from "../../components/productDetail";
 import ProductAnnouncement from "../../components/productAnnouncement";
 import ProductQNA from "../../components/productQNA";
@@ -59,35 +60,8 @@ const Detail = ({fund}) => {
     <main className="flex flex-col gap-[4rem]">
       
       {/* 펀드 상품 상단정보가 들어갈 위치 */}
-      <header className="flex flex-row justify-center gap-[10rem]">
-        <section>
-          <Image src={fund.col1} alt="thumbnail" width={300} height={300} />
-        </section>
-        <aside className="flex flex-col gap-[2rem] w-96">
-          <h2 className="text-center">유명 음식점에서 맛보던 멘보샤를 집에서!</h2>
-          <div>
-            <div className="w-full mt-4 bg-gray-200 rounded-full dark:bg-gray-700">
-              <div className="p-1 rounded-full bg-theme-color" style={{width:'45%'}}></div>
-            </div>
-            <p>45% 달성 (100,000원)</p>
-          </div>
-          <p>겉바속촉의 정석! 유명 식당에서 눈치 봐가며 시키던 멘보샤를 집에서도 푸짐하게 즐기세요! 홈메이드 칠리소스도 함께 드립니다:)</p>
-          <div className="flex flex-row justify-evenly gap-[3rem]">
-            <button className="w-48 py-[0.5rem] bg-theme-color text-white font-black antialiased text-xl justify-self-center">펀딩하기</button>
-            <button className="w-48 py-[0.5rem] border-2 text-gray-600 font-black antialiased text-xl justify-self-center "><span className="text-gray-400">♡ </span>찜하기</button>
-
-          </div>
-          <div className="flex flex-row justify-evenly">
-            <p className="basis-1/2">파트너 정보:
-              <button>블루샹하이</button>
-            </p>
-            <div className="flex flex-row justify-end gap-[1rem] basis-1/2">
-              {/* <button onClick={toggleFollow} className="w-[4.5rem] py-[1px] antialiased bg-gray-200 border-2 justify-self-center rounded-lg">팔로우</button> */}
-              <button className="w-[5rem] py-[1px] antialiased text-white bg-theme-color justify-self-center rounded-lg">알림받기</button>
-            </div>
-          </div>
-        </aside>
-      </header>
+      {/* 소스 링크 같은 경우는 prop 이름이 src 여야한다는 슬픈 사실.. */}
+      <ProductBasics src={fund.col1} />
       <hr />
       {/* 상품 상세정보의 네비게이션바  */}
       <nav className="flex flex-row gap-[3rem] justify-center mr-[50rem]">
