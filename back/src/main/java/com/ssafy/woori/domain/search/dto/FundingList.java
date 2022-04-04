861b41b0961d57475aaca9d6cdfc4a20b67a9208
pangE <cdpkct@gmail.com>
2022-04-04 16:29:23 +0900

    Feat: add 501 API

---
+++ back/src/main/java/com/ssafy/woori/domain/search/dto/FundingList.java
@@ -0,0 +1,9 @@
+package com.ssafy.woori.domain.search.dto;
+
+public interface FundingList {
+    String getFundingSeq();
+    String getFundingImage();
+    String getFundingTitle();
+    String getFundingSimple();
+    //String getFundingContract();
+}
