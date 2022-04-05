620089ddf674c641ebec688bb32b58ff1079022e
fbeotjd <fb1014@naver.com>
2022-04-05 14:43:33 +0900

    test: patch method

--- back/src/main/java/com/ssafy/woori/domain/user/controller/UserController.java
+++ back/src/main/java/com/ssafy/woori/domain/user/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -54,13 +55,13 @@ public class UserController {
     	return new ResponseEntity<UserInfoResponse>(response, HttpStatus.OK);
     }
 
-    @PutMapping
+    @PatchMapping
     public ResponseEntity<User> updateUser(@RequestBody UserUpdateRequest request){
     	
     	return new ResponseEntity<User>(userService.updateUser(request), HttpStatus.OK);
     }
     
-    @PutMapping("/profileImage/{userSeq}")
+    @PatchMapping("/profileImage/{userSeq}")
     public ResponseEntity<String> updateUserProfileImage(
     		@RequestPart(value="file", required = false) MultipartFile[] file,
     		@PathVariable int userSeq){
