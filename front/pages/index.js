fa38423785f73cff11736dcaddbebfdc5b25a46f
KIM Tae Hun <gift2barry@gmail.com>
2022-04-07 16:16:06 +0900

    test: User state confirmation

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,30 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { UserContext } from '../lib/UserContext';
-import Send from '../lib/Send';
-import { useEffect, useContext } from 'react';
+
 
 export default function Home() {
-
-  // const {userInfo, setUserInfo} = useContext(UserContext);
-
-  // useEffect(() => {
-  //   Send.get('/user/check')
-  //   .then((data) => {
-  //     if (data.data) {
-  //       console.log(0, data.json());
-  //     }
-  //     console.log(1, data);
-  //     console.log(2, data.data);
-  //     if (data.data) {
-  //       setUserInfo(data.data);
-  //     }
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   })
-  // })
-  
 
   return (
     <div className={styles.container}>
@@ -32,8 +10,6 @@ export default function Home() {
         <title>우리두레</title>
         <link rel="icon" href="/logo.png" />
       </Head>
-      {/* {userInfo ? <h2>로그인 성공</h2> : <h2>로그인 실패</h2>}
-      <h2>wow</h2> */}
     </div>
   )
 }
