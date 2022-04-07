73e472db7f3a8f4406b1f81a1f58ca79b7b705e5
SSAFY <gksrnrdmlsk@naver.com>
2022-04-07 11:39:17 +0900

    fix: Fix youtube address

--- front/pages/intro.js
+++ front/pages/intro.js
@@ -5,10 +5,10 @@ export default function intro() {
   return (
     <div style={{margin: "auto", marginBottom: "200px", width: "70%"}}>
       <Fade>
-        <h1 className="text-4xl antialiased font-semibold text-left text-gray-700 mb-10">판매자를 어떻게 믿어요 ㅠ</h1>
+        <h1 className="text-4xl antialiased font-semibold text-left text-gray-700 mb-10">"판매자를 어떻게 믿어요 ㅠㅠ"</h1>
       </Fade>
-      <Slide direction={'right'}>
-        <h1 className="text-4xl antialiased font-semibold text-right text-gray-700 mb-40">또 플랫폼은요?</h1>
+      <Slide direction={'up'}>
+        <h1 className="text-4xl antialiased font-semibold text-right text-gray-700 mb-40">"또 플랫폼은요?"</h1>
       </Slide>
       <Flip>
         <h1 className="text-4xl antialiased font-semibold text-center text-gray-700 mb-10">.</h1>
@@ -34,7 +34,7 @@ export default function intro() {
       <Fade fraction={0.8}>
         <h1 className="text-4xl antialiased font-bold text-left text-gray-700 mb-20">1. 메타마스크 지갑을 준비해요.</h1>
       </Fade>
-      <iframe width="800" height="400" src="https://www.youtube.com/embed/dHANMtxLXt0" title="YouTube video player" frameㅠorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="800" height="400" src="https://www.youtube.com/embed/dHANMtxLXt0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
       <h1 className="text-4xl mt-60 antialiased font-bold text-right text-gray-700 mb-20">2. 네이버, 카카오 <br/> 어떤 것이든 이용하실 수 있어요.</h1>
       <div style={{margin: "auto", marginBottom: "2rem", marginRight: "-30px", float: "right"}}>
